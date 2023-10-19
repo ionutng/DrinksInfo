ab66f81c91e343252fb33e82a338540c5fedc2e2
ionutng <ionutngp@gmail.com>
2023-10-19 11:18:17 +0300

    View the categories

---
+++ DrinksInfo/Category.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo
+{
+    internal class Category
+    {
+        public string strCategory { get; set; }
+    }
+    
+    internal class Categories
+    {
+        [JsonProperty("drinks")]
+        public List<Category> CategoriesList { get; set; }
+    }
+}
---
+++ DrinksInfo/DrinksService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using ConsoleTableExt;
+
+namespace DrinksInfo
+{
+    internal class DrinksService
+    {
+        internal void GetCategories()
+        {
+            var client = new RestClient("https://www.thecocktaildb.com/api/json/v1/1/");
+            var request = new RestRequest("list.php?c=list");
+            var response = client.Get(request);
+
+            var serialize = JsonConvert.DeserializeObject<Categories>(response.Content);
+
+            List<Category> returnedList = serialize.CategoriesList;
+
+            Console.Clear();
+
+            ConsoleTableBuilder
+                .From(returnedList)
+                .WithTitle("Categories Menu", ConsoleColor.Red)
+                .WithColumn()
+                .ExportAndWriteLine();
+            Console.WriteLine("\n\n");
+        }
+    }
+}
